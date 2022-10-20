f63efafaca90fe17b421612fc97cad511f5cefd6
natbabo1 <rutsarun.lim@gmail.com>
2022-10-20 15:53:18 +0700

    fix: add .sequelizerc to solve cli bug

--- src/app.js
+++ src/app.js
@@ -1,31 +1,32 @@
-require('dotenv').config();
+require("dotenv").config();
 
 // -----------sync to table -----------------
-// const { sequelize } = require('./src/models');
+// const { sequelize } = require("./models");
 // sequelize.sync({ force: true });
 // -----------End sync to table -------------
 
-const express = require('express');
-const app = express();
-const cors = require('cors');
-const morgan = require('morgan');
-const notFound = require('./middlewares/notFound');
-const error = require('./middlewares/error');
-const authRoute = require('./routes/authRoute');
-const categoryRoute = require('./routes/categoryRoute');
-const userRoute = require('./routes/userRoute');
-const authenticate = require('./middlewares/authenticate');
+const express = require("express");
+const cors = require("cors");
+const morgan = require("morgan");
+const notFound = require("./middlewares/notFound");
+const error = require("./middlewares/error");
+const authRoute = require("./routes/authRoute");
+const categoryRoute = require("./routes/categoryRoute");
+const userRoute = require("./routes/userRoute");
+const authenticate = require("./middlewares/authenticate");
 
-if (process.env.NODE_ENV === 'development') {
-  app.use(morgan('dev')); // ใช้เพื่อดู req ใน nodemon
+const app = express();
+
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev")); // ใช้เพื่อดู req ใน nodemon
 }
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.use('/auth', authRoute);
-app.use('/category', categoryRoute);
+app.use("/auth", authRoute);
+app.use("/category", categoryRoute);
 // app.use('/users', authenticate, userRoute);
 app.use(notFound);
 app.use(error);
