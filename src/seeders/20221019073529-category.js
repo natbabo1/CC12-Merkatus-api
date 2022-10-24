0d314400b91e5a54a13bd387c70d34caf2ac1d7b
natbabo1 <rutsarun.lim@gmail.com>
2022-10-24 13:42:10 +0700

    fix: change catedory image in seed

--- src/seeders/20221019073529-category.js
+++ src/seeders/20221019073529-category.js
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 
 module.exports = {
   async up(queryInterface, Sequelize) {
@@ -12,119 +12,119 @@ module.exports = {
      * }], {});
      */
 
-    return queryInterface.bulkInsert('categories', [
+    return queryInterface.bulkInsert("categories", [
       {
-        category_name: 'เสื้อผ้าชาย',
+        category_name: "เสื้อผ้าชาย",
         image:
-          'https://res.cloudinary.com/dkbiwdyij/image/upload/v1666174435/1_yqidwe.png',
+          "https://res.cloudinary.com/dkbiwdyij/image/upload/v1666174435/Category/1_yqidwe.png",
         created_at: new Date(),
-        updated_at: new Date(),
+        updated_at: new Date()
       },
       {
-        category_name: 'เสื้อผ้าหญิง',
+        category_name: "เสื้อผ้าหญิง",
         image:
-          'https://res.cloudinary.com/dkbiwdyij/image/upload/v1666174435/2_inqftc.png',
+          "https://res.cloudinary.com/dkbiwdyij/image/upload/v1666174435/Category/2_inqftc.png",
         created_at: new Date(),
-        updated_at: new Date(),
+        updated_at: new Date()
       },
       {
-        category_name: 'มือถือและอุปกรณ์',
+        category_name: "มือถือและอุปกรณ์",
         image:
-          'https://res.cloudinary.com/dkbiwdyij/image/upload/v1666174435/3_vecnmc.png',
+          "https://res.cloudinary.com/dkbiwdyij/image/upload/v1666174435/Category/3_vecnmc.png",
         created_at: new Date(),
-        updated_at: new Date(),
+        updated_at: new Date()
       },
       {
-        category_name: 'เสริมความงามและของใช้ส่วนตัว',
+        category_name: "เสริมความงามและของใช้ส่วนตัว",
         image:
-          'https://res.cloudinary.com/dkbiwdyij/image/upload/v1666174435/5_annlkm.png',
+          "https://res.cloudinary.com/dkbiwdyij/image/upload/v1666174435/Category/5_annlkm.png",
         created_at: new Date(),
-        updated_at: new Date(),
+        updated_at: new Date()
       },
       {
-        category_name: 'อาหารเสริมและผลิตภัณฑ์สุขภาพ',
+        category_name: "อาหารเสริมและผลิตภัณฑ์สุขภาพ",
         image:
-          'https://res.cloudinary.com/dkbiwdyij/image/upload/v1666174435/6_xm33sj.png',
+          "https://res.cloudinary.com/dkbiwdyij/image/upload/v1666174435/Category/6_xm33sj.png",
         created_at: new Date(),
-        updated_at: new Date(),
+        updated_at: new Date()
       },
       {
-        category_name: 'ของเล่นและสินค้าแม่และเด็ก',
+        category_name: "ของเล่นและสินค้าแม่และเด็ก",
         image:
-          'https://res.cloudinary.com/dkbiwdyij/image/upload/v1666174435/4_ircw3t.png',
+          "https://res.cloudinary.com/dkbiwdyij/image/upload/v1666174435/Category/4_ircw3t.png",
         created_at: new Date(),
-        updated_at: new Date(),
+        updated_at: new Date()
       },
       {
-        category_name: 'นาฬิกาและแว่นตา',
+        category_name: "นาฬิกาและแว่นตา",
         image:
-          'https://res.cloudinary.com/dkbiwdyij/image/upload/v1666174436/7_tjv8va.png',
+          "https://res.cloudinary.com/dkbiwdyij/image/upload/v1666174436/Category/7_tjv8va.png",
         created_at: new Date(),
-        updated_at: new Date(),
+        updated_at: new Date()
       },
       {
-        category_name: 'เครื่องใช้ภายในบ้าน',
+        category_name: "เครื่องใช้ภายในบ้าน",
         image:
-          'https://res.cloudinary.com/dkbiwdyij/image/upload/v1666174435/8_nit9l3.png',
+          "https://res.cloudinary.com/dkbiwdyij/image/upload/v1666174435/Category/8_nit9l3.png",
         created_at: new Date(),
-        updated_at: new Date(),
+        updated_at: new Date()
       },
       {
-        category_name: 'คอมพิวเตอร์และแลปทอป',
+        category_name: "คอมพิวเตอร์และแลปทอป",
         image:
-          'https://res.cloudinary.com/dkbiwdyij/image/upload/v1666174436/9_ozfhrs.png',
+          "https://res.cloudinary.com/dkbiwdyij/image/upload/v1666174436/Category/9_ozfhrs.png",
         created_at: new Date(),
-        updated_at: new Date(),
+        updated_at: new Date()
       },
       {
-        category_name: 'กล้องถ่ายภาพและอุปกรณ์เสริม',
+        category_name: "กล้องถ่ายภาพและอุปกรณ์เสริม",
         image:
-          'https://res.cloudinary.com/dkbiwdyij/image/upload/v1666174436/10_dedsct.png',
+          "https://res.cloudinary.com/dkbiwdyij/image/upload/v1666174436/Category/10_dedsct.png",
         created_at: new Date(),
-        updated_at: new Date(),
+        updated_at: new Date()
       },
       {
-        category_name: 'เครื่องประดับ',
+        category_name: "เครื่องประดับ",
         image:
-          'https://res.cloudinary.com/dkbiwdyij/image/upload/v1666258917/12_qd6jus.png',
+          "https://res.cloudinary.com/dkbiwdyij/image/upload/v1666258917/Category/12_qd6jus.png",
         created_at: new Date(),
-        updated_at: new Date(),
+        updated_at: new Date()
       },
       {
-        category_name: 'กีฬาและกิจกรรมกลางแจ้ง',
+        category_name: "กีฬาและกิจกรรมกลางแจ้ง",
         image:
-          'https://res.cloudinary.com/dkbiwdyij/image/upload/v1666258916/13_axrdb6.png',
+          "https://res.cloudinary.com/dkbiwdyij/image/upload/v1666258916/Category/13_axrdb6.png",
         created_at: new Date(),
-        updated_at: new Date(),
+        updated_at: new Date()
       },
       {
-        category_name: 'เครื่องใช้ไฟฟ้า',
+        category_name: "เครื่องใช้ไฟฟ้า",
         image:
-          'https://res.cloudinary.com/dkbiwdyij/image/upload/v1666258916/14_zmy4ie.png',
+          "https://res.cloudinary.com/dkbiwdyij/image/upload/v1666258916/Category/14_zmy4ie.png",
         created_at: new Date(),
-        updated_at: new Date(),
+        updated_at: new Date()
       },
       {
-        category_name: 'สัตว์เลี้ยง',
+        category_name: "สัตว์เลี้ยง",
         image:
-          'https://res.cloudinary.com/dkbiwdyij/image/upload/v1666258916/15_sidhym.png',
+          "https://res.cloudinary.com/dkbiwdyij/image/upload/v1666258916/Category/15_sidhym.png",
         created_at: new Date(),
-        updated_at: new Date(),
+        updated_at: new Date()
       },
       {
-        category_name: 'ยานยนต์',
+        category_name: "ยานยนต์",
         image:
-          'https://res.cloudinary.com/dkbiwdyij/image/upload/v1666258916/17_pftd9s.png',
+          "https://res.cloudinary.com/dkbiwdyij/image/upload/v1666258916/Category/17_pftd9s.png",
         created_at: new Date(),
-        updated_at: new Date(),
+        updated_at: new Date()
       },
       {
-        category_name: 'เครื่องเขียนและดนตรี',
+        category_name: "เครื่องเขียนและดนตรี",
         image:
-          'https://res.cloudinary.com/dkbiwdyij/image/upload/v1666258916/16_pizqpq.png',
+          "https://res.cloudinary.com/dkbiwdyij/image/upload/v1666258916/Category/16_pizqpq.png",
         created_at: new Date(),
-        updated_at: new Date(),
-      },
+        updated_at: new Date()
+      }
     ]);
   },
 
@@ -135,6 +135,6 @@ module.exports = {
      * Example:
      * await queryInterface.bulkDelete('People', null, {});
      */
-    return queryInterface.bulkDelete('categories', null, {});
-  },
+    return queryInterface.bulkDelete("categories", null, {});
+  }
 };
